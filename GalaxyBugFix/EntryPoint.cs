2a2497947fe9edf7963d6b05d8a5abb4effa15da
Dinorush <onparnorthstar@gmail.com>
2025-11-02 23:35:16 -0500

    Fix certain portals with too high bottom planes

--- GalaxyBugFix/EntryPoint.cs
+++ GalaxyBugFix/EntryPoint.cs
@@ -1,10 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using GalaxyBugFix.Patches;
+using HarmonyLib;
 
 namespace GalaxyBugFix
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.0")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.1")]
     internal sealed class EntryPoint : BasePlugin
     {
         public const string MODNAME = "GalaxyBugFix";
@@ -12,6 +13,7 @@ namespace GalaxyBugFix
         public override void Load()
         {
             VolumeNativePatch.Init();
+            new Harmony(MODNAME).PatchAll();
             Log.LogMessage("Loaded " + MODNAME);
         }
     }
---
+++ GalaxyBugFix/Patches/PortalPatch.cs
@@ -0,0 +1,18 @@
+﻿using CullingSystem;
+using HarmonyLib;
+
+namespace GalaxyBugFix.Patches
+{
+    [HarmonyPatch]
+    internal static class PortalPatch
+    {
+        [HarmonyPatch(typeof(C_Portal), nameof(C_Portal.SetupEdges))]
+        [HarmonyPostfix]
+        private static void FixBottomPlane(C_Portal __instance)
+        {
+            var plane = __instance.m_portalPlanes[2];
+            plane.m_Distance += 0.5f;
+            __instance.m_portalPlanes[2] = plane;
+        }
+    }
+}
